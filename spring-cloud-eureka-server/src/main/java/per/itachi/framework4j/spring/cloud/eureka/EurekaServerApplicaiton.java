b94f68e810c64957145a54aa10995fd9a31762da
ryougiitachi <ryougiitachi@hotmail.com>
2022-05-07 22:55:47 +0800

    [spring-cloud-eureka-server] Initialized project.

---
+++ spring-cloud-eureka-server/src/main/java/per/itachi/framework4j/spring/cloud/eureka/EurekaServerApplicaiton.java
@@ -0,0 +1,15 @@
+package per.itachi.framework4j.spring.cloud.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplicaiton {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplicaiton.class, args);
+    }
+
+}
