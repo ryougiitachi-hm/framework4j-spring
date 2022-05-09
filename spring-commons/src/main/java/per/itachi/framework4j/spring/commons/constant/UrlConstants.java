23b3740440ed9843d92612e3a381a8ae2c3c8662
ryougiitachi <ryougiitachi@hotmail.com>
2022-05-09 21:38:45 +0800

    [spring-commons] Initialized project as common module.

---
+++ spring-commons/src/main/java/per/itachi/framework4j/spring/commons/constant/UrlConstants.java
@@ -0,0 +1,10 @@
+package per.itachi.framework4j.spring.commons.constant;
+
+public class UrlConstants {
+
+    public static final String CUSTOMER_SVC_CUSTOMER_BY_ID_CARD_V1 = "/v1/customers/idCard/{idCard}";
+
+    public static final String CUSTOMER_SVC_CUSTOMER_BY_CUST_NBR_V1 = "/v1/customers/customerNbr/{customerNbr}";
+
+    private UrlConstants() {}
+}
