2647ead6eb77b5e368d10eb615eb61bd325a5386
ryougiitachi <ryougiitachi@hotmail.com>
2022-05-09 23:07:10 +0800

    [spring-cloud-openfeign] Improved project.

--- spring-cloud-openfeign/src/main/java/per/itachi/framework4j/spring/cloud/openfeign/infra/restful/config/RestfulConfig.java
+++ spring-cloud-openfeign/src/main/java/per/itachi/framework4j/spring/cloud/openfeign/infra/restful/config/RestfulConfig.java
@@ -1,9 +1,16 @@
 package per.itachi.framework4j.spring.cloud.openfeign.infra.restful.config;
 
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.loadbalancer.core.ReactorLoadBalancer;
+import org.springframework.cloud.loadbalancer.core.RoundRobinLoadBalancer;
+import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
+import org.springframework.cloud.loadbalancer.support.LoadBalancerClientFactory;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -14,5 +21,21 @@ public class RestfulConfig {
     @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Profile("random")
+    @Bean
+    public ReactorLoadBalancer<ServiceInstance> loadBalancerPolicyForRandom(
+            Environment environment, LoadBalancerClientFactory factory) {
+        String name = environment.getProperty(LoadBalancerClientFactory.PROPERTY_NAME);
+        return new RoundRobinLoadBalancer(factory.getProvider(name, ServiceInstanceListSupplier.class), name);
+    }
+
+    @Profile("roundrobin")
+    @Bean
+    public ReactorLoadBalancer<ServiceInstance> loadBalancerPolicyForRoundRobin(
+            Environment environment, LoadBalancerClientFactory factory) {
+        String name = environment.getProperty(LoadBalancerClientFactory.PROPERTY_NAME);
+        return new RoundRobinLoadBalancer(factory.getProvider(name, ServiceInstanceListSupplier.class), name);
     }
 }
